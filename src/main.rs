19241137d31a381fbcb86f8671f046e1760a5320
t-tajiri <swallow.ava@gmail.com>
2019-11-26 22:18:34 +0900

    add ping pong server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate websocket;
+
+use std::thread;
+use websocket::server::sync::Server;
+use websocket::OwnedMessage;
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::bind("localhost:2794").unwrap();
+
+    for request in server.filter_map(Result::ok) {
+        thread::spawn(|| {
+            if !request.protocols().contains(&"rust-websocket".to_string()) {
+                request.reject().unwrap();
+                return;
+            }
+
+            let mut client = request.use_protocol("rust-websocket").accept().unwrap();
+
+            let ip = client.peer_addr().unwrap();
+
+            println!("Connection from {}", ip);
+
+            let message = OwnedMessage::Text("Hello".to_string());
+            client.send_message(&message).unwrap();
+
+            let (mut receiver, mut sender) = client.split().unwrap();
+
+            for message in receiver.incoming_messages() {
+                let message = message.unwrap();
+
+                match message {
+                    OwnedMessage::Close(_) => {
+                       let message = OwnedMessage::Close(None);
+                       sender.send_message(&message).unwrap();
+                       println!("Client {} disconnected", ip);
+                    }
+                    OwnedMessage::Ping(ping) => {
+                        let message = OwnedMessage::Pong(ping);
+                        sender.send_message(&message).unwrap();
+                    }
+                    _ => sender.send_message(&message).unwrap(),
+                }
+            }
+        });
+    }
 }
